e100ce6e6d15e5c77333c4ef5c96ec5984dd8779
Austin Wang <austinwang@Austins-MBP>
2020-10-24 12:39:52 -0700

    dnase training plots

--- scripts/training_statistics.py
+++ scripts/training_statistics.py
@@ -63,7 +63,7 @@ def get_best_metric_at_best_epoch(models_path, metric_name, reduce_func, compare
     # Get the best value
     best_run, best_epoch, best_val, all_vals = None, None, None, {}
     for run_num in metrics.keys():
-        print(metrics[run_num].keys()) ####
+        # print(metrics[run_num].keys()) ####
         try:
             # Find the best epoch within that run
             best_epoch_in_run, best_val_in_run = None, None
