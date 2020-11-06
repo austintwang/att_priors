89d21cc95c9a024d69ef05fe3e0271a3704d5552
Austin Wang <austinwang@Austins-MBP>
2020-11-05 23:11:33 -0800

    peak significances

--- src/feature/make_profile_transfer_dataset.py
+++ src/feature/make_profile_transfer_dataset.py
@@ -233,6 +233,7 @@ class SamplingCoordsBatcher(torch.utils.data.sampler.Sampler):
             for pos_coords_bed_trans in pos_coords_beds_trans:
                 peaks_table_trans = self._import_peaks(pos_coords_bed_trans)
                 selects = peaks_table_trans.loc[cmp_fn(peaks_table_trans["pval"], sig_thresh)]
+                print(selects) ####
                 for peak in selects:
                     chrom = peak["chrom"]
                     start = peak["peak_start"]
