83770377def74b4106e3959dfaa3373f0c584cbc
Austin Wang <austinwang@Austins-MBP>
2020-12-10 14:07:29 -0800

    motif ablation scores

--- src/extract/compute_ablate.py
+++ src/extract/compute_ablate.py
@@ -242,10 +242,10 @@ def run(files_spec, model_path, reference_fasta, model_class, out_path, num_runs
 
         if model_class == "prof_trans":
             seqs, profiles, profs_trans = input_func(peaks_slice)
-            print(profs_trans.shape) ####
-            print(profiles.shape) ####
             profs_trans = profs_trans[:, :num_tasks]
             profs_ctrls = profiles[:, num_tasks:]
+            print(profs_trans.shape) ####
+            print(profiles.shape) ####
             seqs_in, profs_ctrls_in, profs_trans_in = get_ablated_inputs(fps_slice, seqs, profs_ctrls, fp_to_seq_slice, fp_to_peak, masks, num_runs, profs_trans=profs_trans)
             profs_preds_logits, counts_preds = run_model(model_path, seqs_in, profs_ctrls_in, fps, model_args_extras=model_args_extras, profs_trans=profs_trans_in)
         else:
