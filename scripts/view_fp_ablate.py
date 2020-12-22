37445114722e4529103086aadd111dd3546a4f52
Austin Wang <austinwang@Austins-MBP>
2020-12-22 00:45:40 -0800

    motif ablation scores

--- scripts/view_fp_ablate.py
+++ scripts/view_fp_ablate.py
@@ -19,8 +19,7 @@ def load_fp_results(results):
     peaks = []
     avg_metrics = {}
     for batch in results:
-        data = zip(batch[i] for i in ("footprints", "peaks", "metrics"))
-        print(batch) ####
+        data = zip(*(batch[i] for i in ("footprints", "peaks", "metrics")))
         for fps_b, peaks_b, metrics_b in data:
             fps.extend(fps_b)
             peaks.extend(peaks_b)
