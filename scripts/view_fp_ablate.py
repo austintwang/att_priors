8ef81fda0930635bf88ef90546eefb71ce3c7799
Austin Wang <austinwang@Austins-MBP>
2020-12-24 00:28:41 -0800

    motif ablation scores

--- scripts/view_fp_ablate.py
+++ scripts/view_fp_ablate.py
@@ -14,27 +14,46 @@ font_manager.fontManager.ttflist.extend(
     )
 )
 
-def load_fp_results(results, metric_names, peak_size):
+def construct_query_table(peaks):
+    table = {}
+    for peak in set(peaks):
+        chrom, start, end = peak
+        start_hash = start // 1000
+        end_hash = end // 1000
+        for pos_hash in range(begin_hash, end_hash + 1):
+            table.setdefault((chrom, pos_hash), set()).add((start, end),)
+
+def query_peak(query_table, peak):
+    chrom, start, end = peak
+    begin_hash = int(start) // 1000
+    end_hash = int(end) // 1000
+    candidates = set()
+    for pos_hash in range(begin_hash, end_hash + 1):
+        candidates |= query_table.get((chrom, pos_hash,), set())
+
+    intersects = {}
+    for c in candidates:
+        start_c, end_c = c
+        if start <= end_c and start_c <= end:
+            # intersects.append((chrom, start_c, end_c),)
+            distance = np.abs((start + end) - (start_c + end_c)) / 2
+            intersects[(chrom, start_c, end_c)] = distance
+
+    return min(intersects, key=intersects.get)
+
+def load_fp_results(results, metric_names):
     fps = []
     peaks = []
     avg_metrics = {}
     for batch in results:
         # print(batch["metrics"]) ####
         fps.extend(batch["footprints"])
-        if peak_size is None:
-            peaks.extend(batch["peaks"])
-        else:
-            for p in batch["peaks"]:
-                chrom, start, end = p
-                center = int(0.5 * (start + end))
-                half_size = int(0.5 * peak_size)
-                start_s = center - half_size
-                end_s = center + peak_size - half_size
-                peaks.append((chrom, start_s, end_s),)
+        peaks.extend(batch["peaks"])
         for k, v in batch["metrics"].items():
             metrics_mean_b = np.mean(v, axis=0)
             avg_metrics.setdefault(k, []).extend(metrics_mean_b)
 
+    peak_table = construct_query_table(peaks)
     results_dict = {"Footprint Coordinates": fps, "Peak Coordinates": peaks}
     # print(avg_metrics.keys()) ####
     metric_names_vars = []
@@ -53,9 +72,9 @@ def load_fp_results(results, metric_names, peak_size):
     # print([(k, v.shape if isinstance(v, np.ndarray) else len(v)) for k, v in results_dict.items()]) ####
     results_df = pd.DataFrame(results_dict)
 
-    return results_df, metric_names_vars
+    return results_df, metric_names_vars, peak_table
 
-def load_enrichments(results_df, genome_prefix, models_path, query_run):
+def load_enrichments(results_df, genome_prefix, models_path, query_run, peak_table):
     epsilon = 1.
     if prefix == "K562_from_HepG2":
         offset = np.log(1400599316) - np.log(1728009533)
@@ -64,7 +83,7 @@ def load_enrichments(results_df, genome_prefix, models_path, query_run):
 
     metrics_path = os.path.join(models_path, f"{prefix}_{query_run}", "metrics.pickle")
     metrics_genome = pd.read_pickle(metrics_path)
-    coords_genome = [tuple(i) for i in metrics_genome["coords"]]
+    coords_genome = [query_peak(peak_table, i) for i in metrics_genome["coords"]]
     # print(coords_genome) ####
     counts_to = metrics_genome["counts_to"].sum(axis=-1)[:,0]
     counts_from = metrics_genome["counts_from"].sum(axis=-1)[:,0]
@@ -99,10 +118,10 @@ def plot_fps(results_df, metric_name, plt_path, sample_size=None):
     plt.savefig(plt_path, bbox_inches='tight')
     plt.clf()
 
-def view_fp_ablate(results_path, plt_dir, models_path, model_query_run, prefix, metric_names, peak_size=None):
+def view_fp_ablate(results_path, plt_dir, models_path, model_query_run, prefix, metric_names):
     results = pd.read_pickle(results_path)["results"]
-    results_df, metric_names_vars = load_fp_results(results, metric_names, peak_size)
-    results_df = load_enrichments(results_df, prefix, models_path, model_query_run)
+    results_df, metric_names_vars, peak_table = load_fp_results(results, metric_names, peak_size)
+    results_df = load_enrichments(results_df, prefix, models_path, model_query_run, peak_table)
     for metric_name in metric_names_vars:
         plt_path = os.path.join(plt_dir, f"{metric_name}.svg")
         plot_fps(results_df, metric_name, plt_path, sample_size=1000)
@@ -139,7 +158,7 @@ if __name__ == '__main__':
                 results_path = os.path.join(ablate_path, f"{res_name}.pickle")
                 plt_dir = os.path.join(out_dir_base, prefix, res_name)
                 os.makedirs(plt_dir, exist_ok=True)
-                view_fp_ablate(results_path, plt_dir, models_path, run_id, prefix, metric_names, peak_size=2114)
+                view_fp_ablate(results_path, plt_dir, models_path, run_id, prefix, metric_names)
 
     # models_path = "/mnt/lab_data2/atwang/models/domain_adapt/dnase/trained_models/baseline/"
     # out_dir_base = "/users/atwang/results/domain_adapt_results/dnase_models_baseline/"
